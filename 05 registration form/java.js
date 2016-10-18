a06d50e1eb6438f4a741fe522fc3ac5fed3c6f33
christinelopez <clopezco@lesley.edu>
2016-10-17 23:24:44 -0400

    added code

--- 05 registration form/java.js
+++ 05 registration form/java.js
@@ -1,33 +1,40 @@
-function myFunction()
+function validateForm()
 {
-    var a=document.getElementsByClassName("myText").value;
+    var firstname=document.getElementById("firstname");
 
-    if (textbox.value.length >= 2)
-    {
-        document.getElementById("name").innerText="Correct!";
+    checklength(firstname,2);
+
+    var email=document.getElementById("email");
+
+    checklength(email,5);
+
+    var password=document.getElementById("password");
+
+    checklength(password,10);
+
+    password.value.search(/(0-9)+/);
+
+    var passwordverification=document.getElementById("passwordverification");
+
+    checkEqual(password,passwordverification);
+}
+function checklength(wordtocheck, minimumlength)  {
+
+    if (wordtocheck.value.length>minimumlength){
+        // The first name is more than 2 characters
+        console.log( wordtocheck.value+" is more than "+minimumlength+" characters");
+
+
+
+    // check to see if object has assaighn to it, if it does remove it
+
+
+
     }
-    else
-    {
-        document.getElementById("name").innerHTML="Oops!";
+    else{
+        // the first name is less than 2 characters
+        console.log( wordtocheck.value+" is less than "+minimumlength+" characters");
+
+        wordtocheck.className = "error";
     }
 }
-//
-//
-// window.changeColour = function(value)
-// {
-//     alert(value);
-//     var color = document.body.style.backgroundColor;
-//     switch(value)
-//     {
-//         case 'b':
-//             color = "#FF0000";
-//             break;
-//         case 'r':
-//             color = "#0000FF";
-//             break;
-//         case 'p':
-//             color = "#FF00FF";
-//             break;
-//     }
-//     document.body.style.backgroundColor = color;
-// }
