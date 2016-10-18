febe3529f6c0c5644163e0c351c7d6cb0c3e0bcc
christinelopez <clopezco@lesley.edu>
2016-10-18 12:50:57 -0400

    finalist

--- 05 registration form/java.js
+++ 05 registration form/java.js
@@ -10,13 +10,22 @@ function validateForm()
 
     var password=document.getElementById("password");
 
-    checklength(password,10);
+    checknumberandlength(password,10);
 
     password.value.search(/(0-9)+/);
 
     var passwordverification=document.getElementById("passwordverification");
 
     checkEqual(password,passwordverification);
+
+    var blueButton=document.getElementById("bluef");
+
+    var redButton=document.getElementById("red");
+
+    var yellowButton=document.getElementById("yellow");
+
+    var orangeButton=document.getElementById("orange");
+
 }
 function checklength(wordtocheck, minimumlength)  {
 
@@ -24,17 +33,42 @@ function checklength(wordtocheck, minimumlength)  {
         // The first name is more than 2 characters
         console.log( wordtocheck.value+" is more than "+minimumlength+" characters");
 
+        wordtocheck.className="correct";
 
 
     // check to see if object has assaighn to it, if it does remove it
-
-
 
     }
     else{
         // the first name is less than 2 characters
         console.log( wordtocheck.value+" is less than "+minimumlength+" characters");
 
+        wordtocheck.className = "error";
+    }
+}
+function checknumberandlength(wordtocheck, minimumlength) {
+    if ( /\d/.test(wordtocheck.value) && (wordtocheck.value.length>minimumlength)){
+        // The first name is more than 2 characters
+        console.log( wordtocheck.value+" contains number ");
+        wordtocheck.className = "correct";
+        // check to see if object has assaighn to it, if it does remove it
+    }
+    else{
+        // the first name is less than 2 charactrs
+        console.log( wordtocheck.value+" does not contain number ");
+        wordtocheck.className = "error";
+    }
+}
+function checkEqual(wordtocheck, wordtocheck2) {
+    if (wordtocheck.value==wordtocheck2.value){
+        // THe first name is more than 2 characters
+        console.log( wordtocheck.value+" equals "+wordtocheck2.value);
+        wordtocheck.className = "correct";
+        // check to see if object has assaighn to it, if it does remove it
+    }
+    else{
+        // the first name is less than 2 charactrs
+        console.log( wordtocheck.value+" does not equal "+wordtocheck2.value);
         wordtocheck.className = "error";
     }
 }
