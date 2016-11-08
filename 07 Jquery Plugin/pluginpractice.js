41faf557d6dab285df7e97fdc2305d238a4afed8
christinelopez <clopezco@lesley.edu>
2016-11-08 13:19:43 -0500

    all work

---
+++ 07 Jquery Plugin/pluginpractice.js
@@ -0,0 +1,15 @@
+
+
+$( document ).ready(function() {
+
+    $("nav").navigation({
+        maxWidth: "375px"
+    });
+    $("nav").on("open.navigation", function() {
+
+    }).on("close.navigation", function() {
+
+    });
+});
+
+
